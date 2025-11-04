bbe3feab17090a2094ec037f8c20be8b9fbe5dc3
Higor <higor.sdds@gmail.com>
2025-11-04 19:07:43 -0300

    alteradi

--- codigos-aulas/codigosa-1.py
+++ codigos-aulas/codigosa-1.py
@@ -1,5 +1,5 @@
 def informacoes(**informacao):
-     for chave,valor in informacao.itens():
-              print (f"{chave}:{informacao}")
+     for chave,valor in informacao.items():
+              print (f"{chave}:{valor}")
 informacoes (nome= "lucas", idade=20, canal= "EletricGames", naturalidade= "Brasileiro ", cpf=17692744726 )
   
